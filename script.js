1ea74d92ae7509e34e9e257521429f1a2aaa0828
Ssosiashvili <sosiashvilisoso@gmail.com>
2021-07-23 19:15:52 +0400

    done 5 tasks

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-// 1. 
+// 2.
 
 function createCell (width, height) {
+    let board = document.querySelector('#board');
     let div = document.createElement ('div'); //შეიქმნა დივ ელემენტი.
     div.classList.add('cell'); // დაემატა კლასიი cell.
     div.style.width = width + 'px'; // მიენიჭა სიგანე.
     div.style.height = height + 'px';//მიენიჭა სიგრძე.
-    div.innerText ='0'; // ჩაიწერა რიცხვი 0.
+    div.innerHTML ='0'; // ჩაიწერა რიცხვი 0.
+
+    board.appendChild(div);
 }
 
-// 2.
+// 3.
 
 function addCells (w, h) {
-    let board = document.querySelector('#board');
     board.style.width = w + 'px';
     board.style.height = h + 'px';
 
-    let calculate = w * h / width * height;
+    let cellQuantity = (w / 100) * (h / 100);
+    
+    for (let i = 0; i < cellQuantity; i++) {
+        createCell (100, 100);
+    }
+    addCounter ();
+}
 
+// 4. 
+
+let startButton = document.querySelector('#start');
+
+startButton.addEventListener('click', function() {
+    addCells (600, 600);
+});
+
+// 5.
+
+function addCounter () {
+    let cells = document.querySelectorAll('.cell')
+    for (let i = 0; i < cells.length; i ++) {
+        cells[i].addEventListener ('click', function() {
+        this.innerHTML = (this.innerHTML * 1) + 1;
+        });
+    }    
 }
